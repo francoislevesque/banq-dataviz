b1446b7fa744d777c2fda958de73f337c58e640a
francoislevesque <francois@witify.io>
2019-01-17 15:08:51 -0500

    first commit

---
+++ assets/scripts/2-line-charts.js
@@ -0,0 +1,63 @@
+"use strict";
+
+/**
+ * Fichier permettant de dessiner les graphiques "focus" et "contexte".
+ */
+
+
+/**
+ * Crée une ligne SVG en utilisant les domaines X et Y spécifiés.
+ * Cette fonction est utilisée par les graphiques "focus" et "contexte".
+ *
+ * @param x               Le domaine X.
+ * @param y               Le domaine Y.
+ * @return d3.svg.line    Une ligne SVG.
+ *
+ * @see https://bl.ocks.org/gordlea/27370d1eea8464b04538e6d8ced39e89      (voir line generator)
+ */
+function createLine(x, y) {
+  return d3.line()
+    .x(d => x(d.date))
+    .y(d => y(d.count))
+    .curve(d3.curveMonotoneX);
+}
+
+/**
+ * Crée le graphique focus.
+ *
+ * @param g         Le groupe SVG dans lequel le graphique doit être dessiné.
+ * @param sources   Les données à utiliser.
+ * @param line      La fonction permettant de dessiner les lignes du graphique.
+ * @param color     L'échelle de couleurs ayant une couleur associée à un nom de rue.
+ */
+function createFocusLineChart(g, sources, line, color) {
+  // TODO: Dessiner le graphique focus dans le groupe "g".
+  // Pour chacun des "path" que vous allez dessiner, spécifier l'attribut suivant: .attr("clip-path", "url(#clip)").
+  g.selectAll('path')
+    .data(sources)
+    .enter()
+    .append('path')
+    .attr('class', 'line')
+    .attr("clip-path", "url(#clip)")
+    .attr('d', d => line(d.values))
+    .attr('id', (d, i) => `line${i}`)
+    .style('stroke', d => color(d.name))
+    .style('stroke-width', (d) => {
+      if (d.name === 'Moyenne') {
+        return 2;
+      }
+      return 1;
+    });
+}
+
+/**
+ * Crée le graphique contexte.
+ *
+ * @param g         Le groupe SVG dans lequel le graphique doit être dessiné.
+ * @param sources   Les données à utiliser.
+ * @param line      La fonction permettant de dessiner les lignes du graphique.
+ * @param color     L'échelle de couleurs ayant une couleur associée à un nom de rue.
+ */
+function createContextLineChart(g, sources, line, color) {
+  
+}
