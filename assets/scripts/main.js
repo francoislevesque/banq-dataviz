f297bfc96ae61a596c3aa0f1dd1a2bfeb18b5612
francoislevesque <francois@witify.io>
2019-01-17 17:14:25 -0500

    Stats analysis

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,125 +1,215 @@
+Promise.all([
+  d3.csv('./data/data.csv'),
+  d3.json('./data/fr-CA.json')
+]).then(initialize);
+
 /**
- * Fichier principal permettant de dessiner les deux graphiques demandés. Ce fichier utilise les autres fichiers
- * que vous devez compléter.
+ * Initialize
  *
- * /!\ Aucune modification n'est nécessaire dans ce fichier!
+ * @param data      Données à utiliser
+ * @param locale    Les paramètres locaux à utiliser
  */
-(function(d3, localization) {
-  "use strict";
-
-  /***** Configuration *****/
-
-  // Graphique principal (focus)
-  var marginFocus = {
-    top: 10,
-    right: 10,
-    bottom: 100,
-    left: 60
-  };
-  var widthFocus = 1200 - marginFocus.left - marginFocus.right;
-  var heightFocus = 500 - marginFocus.top - marginFocus.bottom;
-
-  // Graphique secondaire qui permet de choisir l'échelle de la visualisation (contexte)
-  var marginContext = {
-    top: 430,
-    right: 10,
-    bottom: 30,
-    left: 60
-  };
-  var widthContext = widthFocus;
-  var heightContext = 500 - marginContext.top - marginContext.bottom;
-
-  /***** Échelles *****/
-  var xFocus = d3.scaleTime().range([0, widthFocus]);
-  var yFocus = d3.scaleLinear().range([heightFocus, 0]);
-
-  var xContext = d3.scaleTime().range([0, widthContext]);
-  var yContext = d3.scaleLinear().range([heightContext, 0]);
-
-  var xAxisFocus = d3.axisBottom(xFocus).tickFormat(localization.getFormattedDate);
-  var yAxisFocus = d3.axisLeft(yFocus);
-
-  var xAxisContext = d3.axisBottom(xContext).tickFormat(localization.getFormattedDate);
-
-  /***** Création des éléments *****/
-  var svg = d3.select("body")
-    .append("svg")
-    .attr("width", widthFocus + marginFocus.left + marginFocus.right)
-    .attr("height", heightFocus + marginFocus.top + marginFocus.bottom);
-
-  // Groupe affichant le graphique principal (focus).
-  var focus = svg.append("g")
-    .attr("transform", "translate(" + marginFocus.left + "," + marginFocus.top + ")");
-
-  // Groupe affichant le graphique secondaire (contexte).
-  var context = svg.append("g")
-    .attr("transform", "translate(" + marginContext.left + "," + marginContext.top + ")");
-
-  // Ajout d'un plan de découpage.
-  svg.append("defs")
-    .append("clipPath")
-    .attr("id", "clip")
-    .append("rect")
-    .attr("width", widthFocus)
-    .attr("height", heightFocus);
-
-  // Fonctions pour dessiner les lignes
+function initialize([data, locale]) {
   
-  var lineFocus = createLine(xFocus, yFocus);
-  var lineContext = createLine(xContext, yContext);
+  d3.timeFormatDefaultLocale(locale);
 
-  // Permet de redessiner le graphique principal lorsque le zoom/brush est modifié.
-  var brush = d3.brushX()
-    .extent([[0, 0], [widthContext, heightContext]])
-    .on("brush", function () {
-      brushUpdate(brush, focus, lineFocus, xFocus, xContext, xAxisFocus, yAxisFocus);
+  var dataUniqueSongs = {};
+
+  for (var i = 0, len = data.length; i < len; i++)
+  dataUniqueSongs[data[i]['Id_Chanson']] = data[i];
+
+  data = new Array();
+  for (var key in dataUniqueSongs)
+    data.push(dataUniqueSongs[key]);
+
+  makeAggregateGraphs(data);
+
+  buildTree(data);
+}
+
+function buildTree(data) {
+
+  let roots = {};
+
+  data.forEach(d => {
+    if (d.Id_Chanson_Parent == "") {
+      roots[d.Id_Chanson] = d;
+    }
+  });
+
+  let adapations = {};
+
+  data.forEach(d => {
+    if (d.Id_Chanson_Parent != "") {
+      adapations[d.Id_Chanson] = d;
+    }
+  });
+
+  childrenStats = [
+    Object.keys(roots).length
+  ];
+
+  childrenStats = linkAdaptations(roots, adapations, childrenStats, 1);
+
+  // Configuration
+  const margin = {
+    top: 20,
+    right: 10,
+    bottom: 10,
+    left: 10
+  };
+
+  const width = 400 - margin.left - margin.right;
+  const height = 400 - margin.top - margin.bottom;
+
+  const svg = d3.select('body')
+    .append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom);
+    
+  const g = svg.append('g')
+    .attr('transform', `translate(${margin.left}, ${margin.top})`);
+  
+  g.selectAll('text')
+    .data(childrenStats)
+    .enter()
+    .append('text')
+    .text((d, i) => `Niveau ${i}: ${d}`)
+    .attr("y", (d, i) => 8 + i * 25)
+    .attr("x", 20);
+
+  g.append('text')
+    .text(`Total: ${data.length}`)
+    .attr("y", (d, i) => 8 + childrenStats.length * 25)
+    .attr("x", 20);
+}
+
+function linkAdaptations(roots, adaptations, childrenStats, deepness) {
+  let newAdaptations = {};
+  let newRoots = {};
+
+  for (let i in adaptations) {
+    let a = adaptations[i];
+    let parent = roots[a.Id_Chanson_Parent];
+    
+    if (parent == undefined) {
+      newAdaptations[a.Id_Chanson] = a;
+    } else {
+      if (childrenStats[deepness] === undefined) {
+        childrenStats[deepness] = 0;
+      }
+      childrenStats[deepness]++;  
+      if (parent.adapations === undefined) {
+        roots[a.Id_Chanson_Parent].adapations = [];
+      }
+      roots[a.Id_Chanson_Parent].adapations.push(a);
+      newRoots[a.Id_Chanson] = a;
+    }
+  }
+
+  deepness++;
+
+  if (Object.keys(newAdaptations).length == 0 || deepness == 10) {
+    return childrenStats;
+  }
+
+  return linkAdaptations(newRoots, newAdaptations, childrenStats, deepness);
+}
+
+function makeAggregateGraphs(data) {
+  var stats = {};
+
+  let aggregatesAttributes = [
+    'Lieux_Activites_Autorite',
+    'Annee_Creation_Chanson',
+    'Langues_Chanson'
+  ];
+
+  aggregatesAttributes.forEach(a => {
+
+    stats[a] = {};
+    
+    data.forEach(d => {
+      if (d[a] !== undefined) {
+        let text = d[a].split(';')[0];
+        if (text != "") {
+          stats[a][text] = {
+            count: (stats[a][text] === undefined) ? 1 : +stats[a][text].count + 1,
+            label: text
+          }
+        }
+      }
     });
 
-  /***** Chargement des données *****/
-  d3.csv("./data/2016.csv").then(function(data) {
+    stats[a] = Object.values(stats[a]);
 
-    /***** Prétraitement des données *****/
-    // Échelle permettant d'associer 10 valeurs à 10 couleurs différentes
-    var color = d3.scaleOrdinal(d3.schemeCategory10);
-
-    domainColor(color, data);
-    parseDate(data);
-
-    var sources = createSources(color, data);
-    domainX(xFocus, xContext, data);
-    //console.log(xFocus(new Date('01/01/2016')))
-    domainY(yFocus, yContext, sources);
-
-    /***** Création du graphique focus *****/
-    createFocusLineChart(focus, sources, lineFocus, color);
-
-    // Axes focus
-    focus.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + heightFocus + ")")
-      .call(xAxisFocus);
-
-    focus.append("g")
-      .attr("class", "y axis")
-      .call(yAxisFocus);
-
-    /***** Création du graphique contexte *****/
-    createContextLineChart(context, sources, lineContext, color);
-
-    // Axes contexte
-    context.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + heightContext + ")")
-      .call(xAxisContext);
-
-    context.append("g")
-      .attr("class", "x brush")
-      .call(brush)
-      .selectAll("rect")
-      .attr("y", -6)
-      .attr("height", heightContext + 7);
-
-    /***** Création de la légende *****/
-    legend(svg, sources, color);
   });
-})(d3, localization);
+
+  stats['Lieux_Activites_Autorite'].sort((a, b) => {
+    return b.count - a.count;
+  });
+
+  stats['Langues_Chanson'].sort((a, b) => {
+    return b.count - a.count;
+  });
+
+  stats['Annee_Creation_Chanson'].sort((a, b) => {
+    return a.label - b.label;
+  });
+
+  aggregatesAttributes.forEach(a => {
+    makeBandGraph(stats[a]);
+  });
+}
+
+function makeBandGraph(dataset) {
+
+  // Configuration
+  const margin = {
+    top: 20,
+    right: 10,
+    bottom: 100,
+    left: 140
+  };
+
+  const width = 400 - margin.left - margin.right;
+  const height = 1800 - margin.top - margin.bottom;
+
+  const x = d3.scaleLinear()
+    .domain([0, d3.max(dataset, c => c.count)])
+    .range([0, width])
+
+  const y = d3.scaleBand()
+    .domain(dataset.map(d => d.label))
+    .range([0, height])
+    .paddingInner([0.1]);
+
+  const xAxis = d3.axisTop(x).ticks(5);
+  const yAxis = d3.axisLeft(y);
+
+  const svg = d3.select('body')
+    .append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom);
+
+  const g = svg.append('g')
+    .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+  g.selectAll('rect.bar')
+    .data(dataset)
+    .enter()
+    .append('rect')
+    .attr("class", "bar")
+    .attr("height", y.bandwidth())
+    .attr("width", d => x(d.count))
+    .attr("x", 0)
+    .attr("y", d => y(d.label));
+
+  g.append('g')
+    .attr('class', 'x axis')
+    .call(xAxis);
+
+  g.append('g')
+    .attr('class', 'y axis')
+    .call(yAxis);
+}
